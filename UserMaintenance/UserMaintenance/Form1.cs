c9bf1b0d5d29f984b42d1d9c60b8ad1527fa430b
Pusztai László <laszlo.pusztai@stud.uni-corvinus.hu>
2022-09-30 10:46:52 +0200

    Fájlba írás

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,8 @@ namespace UserMaintenance
         {
             InitializeComponent();
             lblLastName.Text = Resource1.FullName;  
-            btnAdd.Text = Resource1.Add; 
+            btnAdd.Text = Resource1.Add;
+            btnFajlba.Text = Resource1.Fajlba;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -34,6 +36,17 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void btnFajlba_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("Adatok.txt"))
+            {
+                foreach (var item in listUsers.Items)
+                {
+                    sw.WriteLine(item.ToString());
+                }
+            } 
         }
     }
 }
--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -70,6 +70,15 @@ namespace UserMaintenance {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Fájlba Írás.
+        /// </summary>
+        internal static string Fajlba {
+            get {
+                return ResourceManager.GetString("Fajlba", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Teljesnév.
         /// </summary>
         internal static string FullName {
