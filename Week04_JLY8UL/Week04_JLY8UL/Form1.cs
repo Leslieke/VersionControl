b817377d664cc89a2650b4b96180c40edb956b22
Pusztai László <laszlo.pusztai@stud.uni-corvinus.hu>
2022-10-07 10:28:20 +0200

    munkafüzet létrehozása

--- Week04_JLY8UL/Week04_JLY8UL/Form1.cs
+++ Week04_JLY8UL/Week04_JLY8UL/Form1.cs
@@ -16,15 +16,53 @@ namespace Week04_JLY8UL
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
-            LoadData();
+            //LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            throw new NotImplementedException();
         }
     }
 }
