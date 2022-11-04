2dc53db7b4c78b2c1062c03faf5126a5debf3110
Pusztai László <laszlo.pusztai@stud.uni-corvinus.hu>
2022-11-04 10:44:06 +0100

    futószalag

---
+++ Week06_JLY8UL/Week06_JLY8UL/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week06_JLY8UL.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
--- Week06_JLY8UL/Week06_JLY8UL/Entities/CarFactory.cs
+++ Week06_JLY8UL/Week06_JLY8UL/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using Week06_JLY8UL.Abstractions;
 
 namespace Week06_JLY8UL.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
