b3325dedce2a6e9447e1b3d12ba1aa42518e2b58
Pusztai László <laszlo.pusztai@stud.uni-corvinus.hu>
2022-11-11 09:52:41 +0100

    Portfolio tárolása

---
+++ Week07_JLY8UL/Week07_JLY8UL/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week07_JLY8UL.Entities
+{
+    public class PortfolioItem
+    {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
