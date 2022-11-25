e6e3bf65a20e3400fc2bc2635808905e15204b21
Pusztai László <laszlo.pusztai@stud.uni-corvinus.hu>
2022-11-25 10:22:38 +0100

    Jelszó validálás teszt

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true),
+            TestCase("abcdABCD", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("Ab1234", false),
+            TestCase("Abcd1234", true),
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            
+            var actualResult = accountController.ValidateEmail(email);
+
+            
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+
+            var actualResult = accountController.ValidatePassword(password);
+
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
