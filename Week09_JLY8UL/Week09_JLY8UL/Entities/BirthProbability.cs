d520f243ce6b3b83ab3585fde7f25a018c5d0d39
Pusztai László <laszlo.pusztai@stud.uni-corvinus.hu>
2022-12-02 09:50:19 +0100

    Osztályok

---
+++ Week09_JLY8UL/Week09_JLY8UL/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week09_JLY8UL.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int Children { get; set; }
+        public double BirthP { get; set; }
+    }
+}
